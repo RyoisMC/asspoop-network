2723ad43309f217bd9d83f1b688a90c70934bef8
Ryois <ryois@ryois.me>
2020-11-23 12:22:07 -0500

    Setup and nav

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <globalNav/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import globalNav from './components/globalNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    globalNav
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+}
+
+#content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 54px;
 }
 </style>
---
+++ src/components/globalNav.vue
@@ -0,0 +1,15 @@
+
+<template>
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">Asspoop Network</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="https://survival.asspoop.com">Survival Map</b-nav-item>
+        <b-nav-item href="https://creative.asspoop.com">Creative Map</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
